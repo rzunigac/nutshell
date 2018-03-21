7ef1694772068659f7802d3ce7591579df097bf8
rzuniga <rzunigac@gmail.com>
2018-03-21 10:59:35 -0300

    Se formatea mensaje de error con estilo bootstrap

--- config/app_form.php
+++ config/app_form.php
@@ -1,5 +1,7 @@
 <?php return [
+	'error' => '<div class="error-message alert alert-danger">{{content}}</div>',
     'inputContainer' => '<div class="form-group col-md-6 {{required}}">{{content}}</div>',
+    'inputContainerError' => '<div class="form-group col-md-6 input {{required}} error">{{content}}{{error}}</div>',
     'input' => '<input class="form-control {{type}}" type="{{type}}" name="{{name}}"{{attrs}}/>',
     'select' => '<select class="form-control" name="{{name}}"{{attrs}}>{{content}}</select>',
 ];
